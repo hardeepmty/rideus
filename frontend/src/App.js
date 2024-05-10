d3395eea94b75d0992308e15607c3044b451b214
HARDEEP MOHANTY <mohanty4raj@gmail.com>
2024-05-11 01:17:47 +0530

    setup ready

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const App = () => {
+  return (
+    <div>
+      <h1>jshvhsjk</h1>
+      <Typography>fjkfsk</Typography>
+    </div>
+  )
+}
+
+export default App
